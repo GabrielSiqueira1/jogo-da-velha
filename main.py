8780c892f261f65f2c3f16ad459de930807fe0ee
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2024-05-15 18:51:57 -0300

    Grid

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+COMPRIMENTO_JANELA = 720
+screen = pygame.display.set_mode((COMPRIMENTO_JANELA, COMPRIMENTO_JANELA))
 clock = pygame.time.Clock()
 running = True
+
+# instância das imagens
+
+def carregar_img(path, resolution):
+    icon = pygame.image.load(path)
+    return pygame.transform.scale(icon, resolution)
 
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
+    for event in pygame.event.get():    
         if event.type == pygame.QUIT:
             running = False
 
@@ -18,6 +25,8 @@ while running:
     screen.fill("purple")
 
     # RENDER YOUR GAME HERE
+    grid = carregar_img('img/grid.png', [COMPRIMENTO_JANELA, COMPRIMENTO_JANELA])
+    screen.blit(grid, (0, 0))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
