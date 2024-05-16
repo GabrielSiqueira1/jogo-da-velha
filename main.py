66284a12c874a37c607a6c4aab8f03d3b7755adb
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2024-05-16 15:18:34 -0300

    Posição

--- main.py
+++ main.py
@@ -14,11 +14,32 @@ def carregar_img(path, resolution):
     icon = pygame.image.load(path)
     return pygame.transform.scale(icon, resolution)
 
-    board = [
-        [None, None, None],
-        [None, None, None],
-        [None, None, None]
-    ]
+# RENDER YOUR GAME HERE
+grid = carregar_img('img/grid.png', [COMPRIMENTO_JANELA, COMPRIMENTO_JANELA])
+ICON_X = carregar_img('img/1.png', [COMPRIMENTO_JANELA//3, COMPRIMENTO_JANELA//3])
+ICON_O = carregar_img('img/0.jpg', [COMPRIMENTO_JANELA//3, COMPRIMENTO_JANELA//3])
+jogador = 0
+tabuleiro = [
+    [None, None, None],
+    [None, None, None],
+    [None, None, None]
+]
+    
+def turno(jogador_atual):
+    coordenada_atual = pygame.math.Vector2(pygame.mouse.get_pos()) // (COMPRIMENTO_JANELA // 3)
+    if pygame.mouse.get_pressed()[0]:
+        coluna, linha = map(int, coordenada_atual)
+        tabuleiro[linha][coluna] = 0 if jogador_atual == 0 else 1
+        global jogador
+        jogador = 1 - jogador
+    
+def carregar_icones():
+    for i, linha in enumerate(tabuleiro):
+        for j, coluna in enumerate(tabuleiro[i]):
+            if tabuleiro[i][j] == 0:
+                screen.blit(ICON_O, (j * (COMPRIMENTO_JANELA // 3), i * (COMPRIMENTO_JANELA // 3)))
+            if tabuleiro[i][j] == 1:
+                screen.blit(ICON_X, (j * (COMPRIMENTO_JANELA // 3), i * (COMPRIMENTO_JANELA // 3)))
 
 while running:
     # poll for events
@@ -28,16 +49,13 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
-
-    # RENDER YOUR GAME HERE
-    grid = carregar_img('img/grid.png', [COMPRIMENTO_JANELA, COMPRIMENTO_JANELA])
-    ICON_X = carregar_img('img/1.png', [COMPRIMENTO_JANELA//3, COMPRIMENTO_JANELA//3])
-    ICON_O = carregar_img('img/0.png', [COMPRIMENTO_JANELA//3, COMPRIMENTO_JANELA//3])
-    screen.blit(grid, (0, 0))
-
     # flip() the display to put your work on screen
     pygame.display.flip()
+    screen.fill("white")
+    screen.blit(grid, (0, 0))
+    turno(jogador)
+    carregar_icones()
+    pygame.event.wait()
 
     clock.tick(60)  # limits FPS to 60
 
