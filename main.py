cf1ec952f20c7fae9a31e24fe679e55443597687
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2024-05-16 14:40:35 -0300

    img O and X

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ def carregar_img(path, resolution):
     icon = pygame.image.load(path)
     return pygame.transform.scale(icon, resolution)
 
+    board = [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None]
+    ]
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -22,10 +28,12 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("white")
 
     # RENDER YOUR GAME HERE
     grid = carregar_img('img/grid.png', [COMPRIMENTO_JANELA, COMPRIMENTO_JANELA])
+    ICON_X = carregar_img('img/1.png', [COMPRIMENTO_JANELA//3, COMPRIMENTO_JANELA//3])
+    ICON_O = carregar_img('img/0.png', [COMPRIMENTO_JANELA//3, COMPRIMENTO_JANELA//3])
     screen.blit(grid, (0, 0))
 
     # flip() the display to put your work on screen
